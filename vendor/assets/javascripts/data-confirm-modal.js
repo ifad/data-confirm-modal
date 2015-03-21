373d08b6fb7c21b1ec52503fd9c252f44bad059f
Roman Dobynda <roman-wb@yandex.ru>
2015-03-21 10:34:20 +0300

    Update data-confirm-modal.js

--- vendor/assets/javascripts/data-confirm-modal.js
+++ vendor/assets/javascripts/data-confirm-modal.js
@@ -138,7 +138,7 @@
     );
 
     // Make sure it's always the top zindex
-    var highest = current = 1050;
+    var highest = current = 10050;
     $('.modal.in').not('#'+id).each(function() {
       current = parseInt($(this).css('z-index'), 10);
       if(current > highest) {
