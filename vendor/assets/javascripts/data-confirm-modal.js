0e6a6be11ee6102784f7aa79572ac62455f789b1
Marcello Barnaba <vjt@openssl.it>
2015-11-13 17:51:12 +0100

    Set a default verifyClass of form-control #32

--- vendor/assets/javascripts/data-confirm-modal.js
+++ vendor/assets/javascripts/data-confirm-modal.js
@@ -42,7 +42,7 @@
     cancel: 'Cancel',
     cancelClass: 'btn-default',
     fade: true,
-    verifyClass: '',
+    verifyClass: 'form-control',
     elements: ['a[data-confirm]', 'button[data-confirm]', 'input[type=submit][data-confirm]'],
     focus: 'commit',
     zIndex: 1050,
