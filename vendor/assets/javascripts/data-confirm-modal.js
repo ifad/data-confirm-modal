c36aebe744c235c55ccb24197d43c2d1617cf85e
timokae <>
2021-04-29 11:59:24 +0200

    Formatting

--- vendor/assets/javascripts/data-confirm-modal.js
+++ vendor/assets/javascripts/data-confirm-modal.js
@@ -181,7 +181,7 @@
       break;
     }
 
-      var modal = $(
+    var modal = $(
       '<div id="'+id+'" class="modal '+fade+'" tabindex="-1" role="dialog" aria-labelledby="'+id+'Label" aria-hidden="true">' +
         '<div class="modal-dialog '+modalClass+'" role="document">' +
           '<div class="modal-content">' +
