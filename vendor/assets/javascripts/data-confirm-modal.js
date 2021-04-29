a04744c4327287fdd5a70a704d173da0466733c5
timokae <>
2021-04-29 11:52:29 +0200

    Move modalClass to modal-dialog

--- vendor/assets/javascripts/data-confirm-modal.js
+++ vendor/assets/javascripts/data-confirm-modal.js
@@ -181,9 +181,9 @@
       break;
     }
 
-    var modal = $(
-      '<div id="'+id+'" class="modal '+modalClass+' '+fade+'" tabindex="-1" role="dialog" aria-labelledby="'+id+'Label" aria-hidden="true">' +
-        '<div class="modal-dialog" role="document">' +
+      var modal = $(
+      '<div id="'+id+'" class="modal '+fade+'" tabindex="-1" role="dialog" aria-labelledby="'+id+'Label" aria-hidden="true">' +
+        '<div class="modal-dialog '+modalClass+'" role="document">' +
           '<div class="modal-content">' +
             '<div class="modal-header">' +
               modalHeader +
